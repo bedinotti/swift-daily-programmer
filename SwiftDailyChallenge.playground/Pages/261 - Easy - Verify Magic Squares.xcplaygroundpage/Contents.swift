2dea0d111c69d4e6b86e8f049614954c292374eb
Chris Downie <cdownie@gmail.com>
2016-04-10 16:36:38 -0600

    261 updated for optional ints

--- SwiftDailyChallenge.playground/Pages/261 - Easy - Verify Magic Squares.xcplaygroundpage/Contents.swift
+++ SwiftDailyChallenge.playground/Pages/261 - Easy - Verify Magic Squares.xcplaygroundpage/Contents.swift
@@ -8,17 +8,14 @@
  */
 import Foundation
 
-// Define the magic square. Play with ArrayLiteralConvertable
 struct MagicSquare {
     let size : Int
-    let linearContents : [Int]
-    let twoDimensionalContents : [[Int]]
+    let linearContents : [Int?]
+    let twoDimensionalContents : [[Int?]]
 }
 
 extension MagicSquare : ArrayLiteralConvertible {
     typealias Element = Int
-    
-
     
     init(arrayLiteral elements: Int...) {
         self.init(withArray: elements)
@@ -29,16 +26,16 @@ extension MagicSquare : ArrayLiteralConvertible {
         let actualSize = Int(ceil(sqrt(Double(elements.count))))
         
         // Fill in any remaining squares if we weren't given sufficient information.
-        let paddedElements : [Int]
+        let paddedElements : [Int?]
         if elements.count == actualSize {
-            paddedElements = elements
+            paddedElements = elements.map { return .Some($0) }
         } else {
             // Right now, pad any missing elements with 0's. Might need to make the contents optionals to support later problems
-            paddedElements = elements + Array(count: actualSize, repeatedValue: 0)
+            paddedElements = elements.map { return .Some($0) } + Array(count: actualSize, repeatedValue: nil)
         }
         
         // Build the 2d contents
-        var twoDimensionalContents : [[Int]] = []
+        var twoDimensionalContents : [[Int?]] = []
         var startingIndex = 0
         actualSize.stride(through: actualSize * actualSize, by: actualSize).forEach { newIndex in
             let subrange = paddedElements[startingIndex..<newIndex]
@@ -48,7 +45,7 @@ extension MagicSquare : ArrayLiteralConvertible {
         
         // Save the properties
         size = actualSize
-        linearContents = elements
+        linearContents = paddedElements
         self.twoDimensionalContents = twoDimensionalContents
     }
     
@@ -64,8 +61,17 @@ extension MagicSquare {
                 // short circuit if we already found an invalid row
                 return false
             }
+            let areAnyValuesNil = row.reduce(true) { (isValidSoFar, value) -> Bool in
+                guard isValidSoFar else {
+                    return false
+                }
+                return value != nil
+            }
+            guard areAnyValuesNil else {
+                return false
+            }
             
-            let rowSum = row.reduce(0, combine: +)
+            let rowSum = row.flatMap({ $0 }).reduce(0, combine: +)
 
             return rowSum == targetSum
         }
@@ -75,22 +81,47 @@ extension MagicSquare {
                 return false
             }
             
+            var noNilsFound = true
             let columnSum = twoDimensionalContents.reduce(0) { (sumSoFar, row) -> Int in
-                return sumSoFar + row[columnIndex]
+                if let value = row[columnIndex] {
+                    return sumSoFar + value
+                } else {
+                    noNilsFound = false
+                    return sumSoFar
+                }
+            }
+            
+            guard noNilsFound else {
+                return false
             }
             
             return columnSum == targetSum
         }
         
+        var noNilsFound = true
         let uphillSum = (0..<size).reduce(0) { (sumSoFar, index) -> Int in
-            return sumSoFar + twoDimensionalContents[index][index]
+            if let value = twoDimensionalContents[index][index] {
+                return sumSoFar + value
+            } else {
+                noNilsFound = false
+                return sumSoFar
+            }
         }
         let downhillSum = (0..<size).reduce(0) { (sumSoFar, index) -> Int in
-            return sumSoFar + twoDimensionalContents[index][size - index - 1]
+            if let value = twoDimensionalContents[index][size - index - 1] {
+                return sumSoFar + value
+            } else {
+                noNilsFound = false
+                return sumSoFar
+            }
         }
+        
+        guard noNilsFound else {
+            return false
+        }
+        
         let isUphillDiagonalValid = uphillSum == targetSum
         let isDownhillDiagonalValid = downhillSum == targetSum
-        
         
         return areAllRowsValid
             && areAllColumnsValid
@@ -119,7 +150,6 @@ let trueInput1 = [8, 1, 6, 3, 5, 7, 4, 9, 2]
 let trueInput2 = [2, 7, 6, 9, 5, 1, 4, 3, 8]
 let falseInput1 = [3, 5, 7, 8, 1, 6, 4, 9, 2]
 let falseInput2 = [8, 1, 6, 7, 5, 3, 4, 9, 2]
-
 
 
 testMethod(verify, withInput: trueInput1, expectingOutput: true)
