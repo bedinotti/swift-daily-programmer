96dd4402f8dafcec9972827c87ed26131a6b0655
Chris Downie <cdownie@gmail.com>
2016-07-21 14:08:20 -0600

    263 is swift version compatible

--- SwiftDailyChallenge.playground/Pages/263 - Shanon Entropy.xcplaygroundpage/Contents.swift
+++ SwiftDailyChallenge.playground/Pages/263 - Shanon Entropy.xcplaygroundpage/Contents.swift
@@ -8,38 +8,37 @@
  */
 import Foundation
 
-    func entropy(input : String) -> Double {
-        let stringLength = input.characters.count
-        let frequency = input.characters.reduce([:]) { (map, char) -> [Character : Int] in
-            var localMap = map
-            if let existingCount = map[char] {
-                localMap[char] = existingCount + 1
-            } else {
-                localMap[char] = 1
-            }
-            
-            return localMap
+func entropy(_ input : String) -> Double {
+    let stringLength = input.characters.count
+    let frequency = input.characters.reduce([:]) { (map, char) -> [Character : Int] in
+        var localMap = map
+        if let existingCount = map[char] {
+            localMap[char] = existingCount + 1
+        } else {
+            localMap[char] = 1
         }
         
-        return -1 * frequency.reduce(0) { (sum, characterCount) -> Double in
-            let (_, count) = characterCount
-            let fractional = Double(count) / Double(stringLength)
-            let term = fractional * log2(fractional)
-            return sum + term
-        }
+        return localMap
     }
-
+    
+    return -1 * frequency.reduce(0) { (sum, characterCount) -> Double in
+        let (_, count) = characterCount
+        let fractional = Double(count) / Double(stringLength)
+        let term = fractional * log2(fractional)
+        return sum + term
+    }
+}
 
 
 entropy("1223334444")
-testMethod(entropy, withInput: "1223334444", expectingOutput: 1.84644, toBeWithin: 0.00001)
-testMethod(entropy, withInput: "Hello, World!", expectingOutput: 3.18083, toBeWithin: 0.00001)
+testMethod(method: entropy, withInput: "1223334444", expectingOutput: 1.84644, toBeWithin: 0.00001)
+testMethod(method: entropy, withInput: "Hello, World!", expectingOutput: 3.18083, toBeWithin: 0.00001)
 
 // Challenge Input
-testMethod(entropy, withInput: "122333444455555666666777777788888888", expectingOutput: 2.794208683, toBeWithin: 0.000000001)
-testMethod(entropy, withInput: "563881467447538846567288767728553786", expectingOutput: 2.794208683, toBeWithin: 0.000000001)
-testMethod(entropy, withInput: "https://www.reddit.com/r/dailyprogrammer", expectingOutput: 4.056198332, toBeWithin: 0.000000001)
-testMethod(entropy, withInput: "int main(int argc, char *argv[])", expectingOutput: 3.866729296, toBeWithin: 0.000000001)
+testMethod(method: entropy, withInput: "122333444455555666666777777788888888", expectingOutput: 2.794208683, toBeWithin: 0.000000001)
+testMethod(method: entropy, withInput: "563881467447538846567288767728553786", expectingOutput: 2.794208683, toBeWithin: 0.000000001)
+testMethod(method: entropy, withInput: "https://www.reddit.com/r/dailyprogrammer", expectingOutput: 4.056198332, toBeWithin: 0.000000001)
+testMethod(method: entropy, withInput: "int main(int argc, char *argv[])", expectingOutput: 3.866729296, toBeWithin: 0.000000001)
 
 
 
--- SwiftDailyChallenge.playground/Sources/TestMethods.swift
+++ SwiftDailyChallenge.playground/Sources/TestMethods.swift
@@ -10,6 +10,16 @@ import Foundation
         ) -> Bool {
         return testMethod(method, withInput:input, expectingOutput: output)
     }
+    
+    public func testMethod<InputType, OutputType : Strideable> (
+        method method: (InputType) -> OutputType,
+        withInput input: InputType,
+        expectingOutput output: OutputType,
+        toBeWithin marginOfError: OutputType.Stride
+        ) -> Bool {
+        return testMethod(method, withInput: input, expectingOutput: output, toBeWithin:marginOfError)
+    }
+    
 #endif
 
 // Use these functions to test your daily problem solutions.
